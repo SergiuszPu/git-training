1350d725cc8b8b9013fbcc17f352efe445287053
SergiuszPu <serson85@gmail.com>
2022-01-07 14:29:18 +0100

    main zmiany

--- scriptOne.js
+++ scriptOne.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-// siema jedennnnnn
-=======
-// siema napewno tu
->>>>>>> 66f8ba6 (sadasd)
-=======
-// siema napewno tu
->>>>>>> 66f8ba6a05db2d3f1cb520e369273607d99f0d24
+//czy teraz jest dobrze ?
--- scriptTwo.js
+++ scriptTwo.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-//siema script 1
-=======
-//siema script 223232
->>>>>>> 66f8ba6 (sadasd)
-=======
-//siema script 223232
->>>>>>> 66f8ba6a05db2d3f1cb520e369273607d99f0d24
+//co tu sie stało?
